21c833b6dabbe870139373fc616cbe1514387dcf
feyisayo1 <45480719+feyisayo1@users.noreply.github.com>
2020-10-10 11:03:03 +0100

    Add files via upload

---
+++ Assignments/Week 1/ogunleyeDavid.js
@@ -0,0 +1,7 @@
+//celsius temperature to be changed
+var celsiusTemp = 37;
+//storing the equation in a variable converter
+var converter = (celsiusTemp * 9/5 + 32);
+let result = converter;
+//logging the converted temp to the console
+console.log(result);
